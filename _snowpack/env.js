01b11d53a7fdce103d501dc4cbf8f8df556c999c
Michal Radecki <mradecki@jwplayer.com>
2021-11-24 16:54:15 +0100

    Updates

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_BASE_URL = "/build/";
+export const SNOWPACK_PUBLIC_BASE_URL = "/ott-web-app/build";
 export const MODE = "production";
 export const NODE_ENV = "production";
 export const SSR = false;
