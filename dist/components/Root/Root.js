70bfb03ec39d61b64d5df8ed5ff5a0f63ec43985
Michal Radecki <mradecki@jwplayer.com>
2021-12-15 11:24:14 +0100

    Updates

--- dist/components/Root/Root.js
+++ dist/components/Root/Root.js
@@ -1,5 +1,5 @@
 import React from "../../../_snowpack/pkg/react.js";
-import {Route, Switch, Redirect, useLocation} from "../../../_snowpack/pkg/react-router-dom.js";
+import {Route, Switch} from "../../../_snowpack/pkg/react-router-dom.js";
 import {useTranslation} from "../../../_snowpack/pkg/react-i18next.js";
 import User from "../../screens/User/User.js";
 import Series from "../../screens/Series/Series.js";
@@ -12,18 +12,11 @@ import ErrorPage from "../ErrorPage/ErrorPage.js";
 import AccountModal from "../../containers/AccountModal/AccountModal.js";
 import About from "../../screens/About/About.js";
 const Root = ({error}) => {
-  const location = useLocation();
   const {t} = useTranslation("error");
   if (error) {
     return /* @__PURE__ */ React.createElement(ErrorPage, {
       title: t("generic_error_heading", "There was an issue loading the application")
     }, /* @__PURE__ */ React.createElement("p", null, t("generic_error_description", "Try refreshing this page or come back later.")));
-  }
-  if (window.html404 && location.pathname !== "/404") {
-    window.html404 = false;
-    return /* @__PURE__ */ React.createElement(Redirect, {
-      to: "/404"
-    });
   }
   return /* @__PURE__ */ React.createElement(Layout, null, /* @__PURE__ */ React.createElement(Switch, null, /* @__PURE__ */ React.createElement(Route, {
     path: "/",
