c81492d33e25a668d250a8019e52a4a079c4f2c6
Michal Radecki <mradecki@jwplayer.com>
2021-12-20 13:10:55 +0100

    Updates

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,6 @@
 export const SNOWPACK_PUBLIC_BASE_URL = "";
 export const SNOWPACK_PUBLIC_GITHUB_PAGES = "true";
+export const SNOWPACK_PUBLIC_GITHUB_PAGES_CUSTOM_DOMAIN = "showcase-github.jwplayer.com";
 export const MODE = "production";
 export const NODE_ENV = "production";
 export const SSR = false;
--- dist/components/Router/Router.js
+++ dist/components/Router/Router.js
@@ -3,7 +3,11 @@ import * as __SNOWPACK_ENV__ from '../../../_snowpack/env.js';
 import React from "../../../_snowpack/pkg/react.js";
 import {BrowserRouter, HashRouter} from "../../../_snowpack/pkg/react-router-dom.js";
 export default function Router(props) {
-  if (__SNOWPACK_ENV__.SNOWPACK_PUBLIC_GITHUB_PAGES) {
+  const {
+    SNOWPACK_PUBLIC_GITHUB_PAGES,
+    SNOWPACK_PUBLIC_GITHUB_PAGES_CUSTOM_DOMAIN
+  } = __SNOWPACK_ENV__;
+  if (SNOWPACK_PUBLIC_GITHUB_PAGES && !SNOWPACK_PUBLIC_GITHUB_PAGES_CUSTOM_DOMAIN) {
     return /* @__PURE__ */ React.createElement(HashRouter, {
       ...props
     });
