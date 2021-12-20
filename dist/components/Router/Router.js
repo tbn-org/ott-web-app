7ff9ec5d4287f5ad31b6992fcbf64adfd7eaa9c0
Michal Radecki <mradecki@jwplayer.com>
2021-12-20 13:21:35 +0100

    Updates

--- dist/components/Router/Router.js
+++ dist/components/Router/Router.js
@@ -3,11 +3,7 @@ import * as __SNOWPACK_ENV__ from '../../../_snowpack/env.js';
 import React from "../../../_snowpack/pkg/react.js";
 import {BrowserRouter, HashRouter} from "../../../_snowpack/pkg/react-router-dom.js";
 export default function Router(props) {
-  const {
-    SNOWPACK_PUBLIC_GITHUB_PAGES,
-    SNOWPACK_PUBLIC_GITHUB_PAGES_CUSTOM_DOMAIN
-  } = __SNOWPACK_ENV__;
-  if (SNOWPACK_PUBLIC_GITHUB_PAGES && !SNOWPACK_PUBLIC_GITHUB_PAGES_CUSTOM_DOMAIN) {
+  if (__SNOWPACK_ENV__.SNOWPACK_PUBLIC_GITHUB_PAGES) {
     return /* @__PURE__ */ React.createElement(HashRouter, {
       ...props
     });
